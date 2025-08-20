c19b56ec45ea4743986ab3c4cf9326f3cbd6442c
HVAksh <chaudharyaksh92@gmail.com>
2025-08-20 23:26:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,14 +38,14 @@ resource "azurerm_linux_virtual_machine" "vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = Standard_B1s
-  admin_username      = var.admin_username
+  admin_username      = azureuser
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
 
   admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
+    username   = azureuser
+    public_key = /var/lib/jenkins/.ssh/id_rsa.pub
   }
 
   os_disk {
